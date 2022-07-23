b3f84d00d71d548baaf74994ee5a352d9dee44d1
Felipe Ramos <felipe.ramos@osf.digital>
2022-07-23 20:27:10 -0300

    Update

--- src/components/contents/slot-details/index.js
+++ src/components/contents/slot-details/index.js
@@ -3,7 +3,29 @@ export default function SlotDetails({ pageData, setPageData }) {
 
     return (
         <div className="container">
-            Slot Details
+            <section className="content-fullwidth">
+                
+            </section>
+
+            <section className="content-sidebar">
+                <div className="content">
+                    <div className="section-header">
+                        <h2>Slot Details</h2>
+                    </div>
+
+                    <div className="section-header">
+                        <h2>Monitor</h2>
+                    </div>
+
+                    <div className="section-header">
+                        <h2>Posições fechadas</h2>
+                    </div>
+                </div>
+
+                <div className="sidebar">
+                    
+                </div>
+            </section>
         </div>
     );
 }
