54b933fe33ee5787ce8b700b92965ee7e951ca1d
Felipe Ramos <felipe.ramos@osf.digital>
2022-10-06 18:08:49 -0300

    Installing Next.js 12

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
-module.exports = {
-  reactStrictMode: true,
+module.exports = async (phase, { defaultConfig }) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    reactStrictMode: true
+  }
+  return nextConfig
 }
