bc399920e4819249c09d319bb6d1b4a03bdd21cd
Felipe Ramos <felipe.ramos@osf.digital>
2023-03-04 18:47:02 -0300

    Updating

--- src/components/headers/mainHeader.js
+++ src/components/headers/mainHeader.js
@@ -1,12 +1,22 @@
+import Link from 'next/link';
+import Badge from '@mui/material/Badge';
+import LogoDevIcon from '@mui/icons-material/LogoDev';
+import IconButton from '@mui/material/IconButton';
+
 export default function MainHeader({pageData}){
-    const user = pageData && pageData.user;
     return (
         <header>
             <div className="container header-wrap">
                 <div className="header-column logo-wrap">
                 </div>
                 <div className="header-column menu-wrap">
-                    Seja bem-vindo {user && user.firstName}
+                    <Link href="/logs" passHref>
+                        <IconButton className="icon-button">
+                            <Badge badgeContent={pageData?.logsCount || 0} color="error">
+                                <LogoDevIcon />
+                            </Badge>
+                        </IconButton>
+                    </Link>
                 </div>
             </div>
         </header>
