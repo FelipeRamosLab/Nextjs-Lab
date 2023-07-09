9dbc382ac9d5bda69a1d1a184b3dbec9a774381b
Felipe Ramos <felipe@feliperamos.info>
2023-07-09 16:24:25 -0300

    [v1] Motor controller

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,75 @@
+import { useState } from 'react';
+
+let timer;
+const MAX_SPEED = 1000;
+const styles = {
+  button: {
+    padding: '10px 30px',
+    margin: '10px'
+  },
+  slide: {
+    width: '100%'
+  }
+};
+
 export default function Home() {
+  const [speed, setSpeed] = useState(MAX_SPEED);
+
+  const handlePower = async (state) => {
+    try {
+      const response = await fetch('http://192.168.15.45/motor/power/' + state, {
+        headers: { 'Content-Type': 'application/json' },
+        method: 'GET'
+      });
+      const data = await response.json();
+  
+      if (data.success) {
+        if (state === 'on') setSpeed(MAX_SPEED);
+        else if (state === 'off') setSpeed(0);
+        console.log('Power: ' + state);
+      } else {
+        throw data;
+      }
+    } catch (err) {
+      throw err.message;
+    }
+  }
+
+  const handleSpeed = (value) => {
+    setSpeed(value);
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(async () => {
+      try {
+        const response = await fetch('http://192.168.15.45/motor/speed/' + value, {
+          headers: { 'Content-Type': 'application/json'},
+          method: 'GET'
+        });
+        const data = await response.json();
+    
+        if (data.success) {
+          console.log('Speed: ' + value);
+        } else {
+          throw data;
+        }
+      } catch (err) {
+        throw err.message;
+      }
+    }, 100);
+  }
+
   return <>
-    <h1>NextJS Lab</h1>
+    <h1>Motor Controller</h1>
+
+    <section>
+      <h2>Power</h2>
+      <button style={styles.button} onClick={() => handlePower('on')}>ON</button>
+      <button style={styles.button} onClick={() => handlePower('off')}>OFF</button>
+    </section>
+
+    <section>
+      <h2>Speed</h2>
+
+      <input style={styles.slide} type="range" step={1} min={0} max={MAX_SPEED} value={speed} onChange={({target}) => handleSpeed(Number(target.value))} />
+    </section>
   </>
 }
