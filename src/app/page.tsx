3e5fbafe005842ee37400e9d831bb4d1faba8c99
FelipeRamosDev <felipe@feliperamos.info>
2025-05-11 23:16:41 -0300

    Finishing the exercise

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,151 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+
+import '@/style/style.scss';
+import { useEffect, useReducer } from 'react';
+import booksBundle from '@/books.json';
+
+type Book = {
+  id: number;
+  title: string;
+  author: string;
+  year: number;
+}
+
+type Filter = {
+  start: boolean;
+  title: string;
+  author: string;
+  year: number;
+  result: Book[]
+}
+
+type ReducerAction = {
+  toStart?: boolean,
+  field?: 'title' | 'author' | 'year';
+  value?: string | number;
+}
+
+const initialState: Filter = {
+  start: false,
+  title: '',
+  author: '',
+  year: 0,
+  result: []
+};
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [ filter, dispatch ] = useReducer((state: Filter, action: ReducerAction) => {
+    const newValue = { ...state };
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    if (state.start) {
+      newValue.start = true;
+      newValue.result = booksBundle;
+
+      return newValue;
+    }
+
+    switch (action.field) {
+      case 'author':
+        newValue.author = String(action.value);
+        break;
+      case 'title':
+        newValue.title = String(action.value);
+        break;
+      case 'year':
+        newValue.year = Number(action.value);
+        break;
+    }
+
+    if (!newValue.author && !newValue.title && !newValue.year) {
+      newValue.result = booksBundle;
+    } else {
+      newValue.result = booksBundle.filter(item => {
+        const authorStore = newValue.author.toLowerCase();
+        const titleStore = newValue.author.toLowerCase();
+        const authorInput = item.author.toLowerCase();
+        const titleInput = item.author.toLowerCase();
+
+        return [
+          (!newValue.author || authorInput.indexOf(authorStore) > -1),
+          (!newValue.title || titleInput.indexOf(titleStore) > -1),
+          (!newValue.year || item.year === newValue.year)
+        ].every(item => item);
+      });
+    }
+
+    return newValue;
+  }, initialState);
+
+  useEffect(() => {
+    dispatch({ toStart: true });
+  }, []);
+  
+  return (
+    <div className="search-box">
+      <div className="inputs">
+        <div className="input-wrap">
+          <label htmlFor="title">
+            Title
+          </label>
+
+          <input type="text" id="title" value={filter.title} onChange={(e) => dispatch({ field: 'title', value: e.target.value })} />
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div className="input-wrap">
+          <label htmlFor="author">
+            Author
+          </label>
+
+          <input type="text" id="author" value={filter.author} onChange={(e) => dispatch({ field: 'author', value: e.target.value })} />
+        </div>
+
+        <div className="input-wrap">
+          <label htmlFor="year">
+            Year
+          </label>
+
+          <input type="number"  id="year" value={filter.year} onChange={(e) => dispatch({ field: 'year', value: e.target.value })} />
+        </div>
+      </div>
+
+      <div className="books-list">
+        {filter.result.map(book => (
+          <div key={book.id} className="book">
+            <div className="line">
+              <span className="label">
+                Title
+              </span>
+              <span className="value">
+                {book.title}
+              </span>
+            </div>
+            <div className="line">
+              <span className="label">
+                Author
+              </span>
+              <span className="value">
+                {book.author}
+              </span>
+            </div>
+            <div className="line">
+              <span className="label">
+                Year
+              </span>
+              <span className="value">
+                {book.year}
+              </span>
+            </div>
+            <div className="line">
+              <span className="label">
+                ID
+              </span>
+              <span className="value">
+                {book.id}
+              </span>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
